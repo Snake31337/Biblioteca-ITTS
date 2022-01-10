04c6733a0c3989d9c257892a3072cc6f051189e3
rhiss <loumb31@gmail.com>
2022-01-10 22:39:05 +0100

    raga ma come si fa???

--- Frontend/src/Components/BookTable.js
+++ Frontend/src/Components/BookTable.js
@@ -1,6 +1,23 @@
 import '../CSS/BookTable.scss';
 
 function BookTable() {
+    var testValues = '[{"CodiceLibro":"1","Titolo":"gigi","Lingua":"inglese","Editore":"mondadori","AnnoPubblicazione":"2012","Categoria":"Fantascienza","ISBN":"sg18303"},{"CodiceLibro":"2","Titolo":"A Game of Thrones","Lingua":"Inglese","Editore":"Mondadori","AnnoPubblicazione":"2012","Categoria":"Fantasy","ISBN":"sg18303"},{"CodiceLibro":"3","Titolo":"A Song of Ice and Fire","Lingua":"Inglese","Editore":"Mondadori","AnnoPubblicazione":"2014","Categoria":"Fantasy","ISBN":"sg18303"},{"CodiceLibro":"4","Titolo":"1984","Lingua":"Inglese","Editore":"Feltrinelli","AnnoPubblicazione":"1948","Categoria":"Distopico","ISBN":"SG18303"}]'
+    //fetch('/api/index.php').then((Response) => Response.text);
+    var libri2;
+    fetch('https://localhost:8080',{
+        method: 'POST',
+        body: {
+          type: 'listBooks',
+        }
+      })
+      .then((response) => response.text())
+      .then((response) => {console.log(response.text)})
+
+    var libri = JSON.parse(testValues);
+    //console.log(libri2);
+    console.log(libri);
+    console.log(libri[1,0]);
+    
     return (
         <div className="table-container">
             <table className="book-table">
@@ -14,20 +31,16 @@ function BookTable() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>The things they carried</td>
-                        <td>Tim O' Brien</td>
-                        <td>IDK</td>
-                        <td>1995</td>
-                        <td>Inglese</td>
-                    </tr>
-                    <tr>
-                        <td>Of mice and men</td>
-                        <td>Leonardo Grimaldi</td>
-                        <td>LOL</td>
-                        <td>2004</td>
-                        <td>Inglese</td>
-                    </tr>   
+                    {libri.map((libro) =>(
+                        <tr>
+                            <td>{libro.Titolo}</td>
+                            <td>{libro.ISBN}</td>
+                            <td>{libro.Editore}</td>
+                            <td>{libro.AnnoPubblicazione}</td>
+                            <td>{libro.Lingua}</td>
+                        </tr>
+                    ))}
+
                 </tbody>
             </table>
         </div>
