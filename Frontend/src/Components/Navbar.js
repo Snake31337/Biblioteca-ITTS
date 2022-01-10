708db84f1296738acb5963cbc01337ef0e81b55e
Louis Marotta <rhiss@tutanota.com>
2022-01-10 17:02:01 +0100

    fix navbar

--- Frontend/src/Components/Navbar.js
+++ Frontend/src/Components/Navbar.js
@@ -1,21 +1,25 @@
 import logo from '../Images/logo.bmp';
 import '../CSS/NavBar.scss';
+import AccountButton from './AccountButton';
+import LanguageSelect from './LanguageSelect';
 
 
 function NavBar() {
     return(
         <nav className="navBar">
-            <div className="container-logo-link">
+            <div className="nav-left nav-items">
+                <LanguageSelect> </LanguageSelect>
+            </div>
+
+            <div className="container-logo-link nav-centered">
                 <a className="link logo-link" href="#">
                     <img className="logo" src={logo} alt="Logo dell'ITTS O.Belluzzi"/>
-                    <p className='logo-text'>Biblioteca ITTS</p>
-                </a>
+                </a>    
             </div>
-            <div className="link-container">
-                <a className="link" href="#">Home</a>
-                <a className="link" href="#">Chi siamo</a>
-                <a className="link" href="#">Lorem</a>
-                <a className="link" href="#">ipsum</a>
+            
+            <div className="nav-right nav-items" > 
+                <button>Tema Scuro</button>
+                <AccountButton> </AccountButton>
             </div>
         </nav>
     );
