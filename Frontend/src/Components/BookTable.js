1f0983f5130347c06679d482b808da125069b744
Louis Marotta <rhiss@tutanota.com>
2022-01-11 09:36:46 +0100

    funziona lo stampamento dei libri, mi viene da piangere

--- Frontend/src/Components/BookTable.js
+++ Frontend/src/Components/BookTable.js
@@ -1,24 +1,44 @@
 import '../CSS/BookTable.scss';
+import React, { Component } from 'react';
 
-function BookTable() {
-    var testValues = '[{"CodiceLibro":"1","Titolo":"gigi","Lingua":"inglese","Editore":"mondadori","AnnoPubblicazione":"2012","Categoria":"Fantascienza","ISBN":"sg18303"},{"CodiceLibro":"2","Titolo":"A Game of Thrones","Lingua":"Inglese","Editore":"Mondadori","AnnoPubblicazione":"2012","Categoria":"Fantasy","ISBN":"sg18303"},{"CodiceLibro":"3","Titolo":"A Song of Ice and Fire","Lingua":"Inglese","Editore":"Mondadori","AnnoPubblicazione":"2014","Categoria":"Fantasy","ISBN":"sg18303"},{"CodiceLibro":"4","Titolo":"1984","Lingua":"Inglese","Editore":"Feltrinelli","AnnoPubblicazione":"1948","Categoria":"Distopico","ISBN":"SG18303"}]'
-    //fetch('/api/index.php').then((Response) => Response.text);
-    var libri2;
-    fetch('https://localhost:8080',{
-        method: 'POST',
-        body: {
+export default class BookTable extends React.Component  {
+
+    constructor(props) {
+        super(props);
+  
+        this.state = {
+          data : []
+        };
+    }
+
+    componentWillMount() {
+        this.renderMyData();
+    }
+
+    renderMyData(){
+        fetch('http://192.168.105.77:8080/', {
+            method: 'POST',
+            body: JSON.stringify({
           type: 'listBooks',
-        }
-      })
-      .then((response) => response.text())
-      .then((response) => {console.log(response.text)})
+        })
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+              this.setState({ data : responseJson })
+            })
+            .then((response) => {console.log(response.json())})
+            .catch((error) => {
+              console.error(error);
+            });
+    }
 
-    var libri = JSON.parse(testValues);
+    //var libri = JSON.parse(testValues);
     //console.log(libri2);
-    console.log(libri);
-    console.log(libri[1,0]);
+    //console.log(libri);
+    //console.log(libri[1,0]);
     
-    return (
+    render (){
+        return(
         <div className="table-container">
             <table className="book-table">
                 <thead>
@@ -31,21 +51,24 @@ function BookTable() {
                     </tr>
                 </thead>
                 <tbody>
-                    {libri.map((libro) =>(
-                        <tr>
+                    {//console.log(this.state.data[0].CodiceLibro)
+                    console.log(this.state.data)
+                    }
+
+                    {
+                        this.state.data.map((libro) => (
+                            <tr>
                             <td>{libro.Titolo}</td>
-                            <td>{libro.ISBN}</td>
+                            <td>{libro.Nome} {libro.Cognome}</td>
                             <td>{libro.Editore}</td>
                             <td>{libro.AnnoPubblicazione}</td>
                             <td>{libro.Lingua}</td>
                         </tr>
-                    ))}
+                        ))
+                    }
 
                 </tbody>
             </table>
         </div>
-    );
+    )}
 }
-
-
-export default BookTable;
