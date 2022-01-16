0c7cdea791b4a4766dff0ea97c5fca93fe1bcae8
rhiss <loumb31@gmail.com>
2022-01-16 10:13:21 +0100

    function button delete object

--- Frontend/src/Components/FunctionButtons.js
+++ Frontend/src/Components/FunctionButtons.js
@@ -1,11 +1,21 @@
-import '../CSS/FunctionButtons.scss'    
-
+import '../CSS/FunctionButtons.scss'; 
+import Modal from './Modal';
 
 
 // Bottoni per l'Editing e cancellazione
-function FunctionButtons() {
+export default function FunctionButtons() {
     function showForm() { // show editing form
         alert("xd");
+    }
+
+    function deleteElement(id){
+        let text = "Sei sicuro di voler cancellare questo elemento?";
+        if(window.confirm(text) == true){
+            alert("Cancellato");
+            //Inserire richiesta di delete qui dentro
+        }else{
+
+        }
     }
     
     return(
@@ -13,7 +23,11 @@ function FunctionButtons() {
                 <button type="button" className="functionButtons-button" onClick={showForm}>
                     <i className="functionButtons-icon bi bi-pen-fill"></i>
                 </button>
-                <button type="button" className="functionButtons-button">
+                {/* Raga per qualche motivo se aggiungo una funzione al pulsante cancella si attiva solo se ci vai sopra la row 
+                    Ho capito, per qualche motivo è solo quando nella funzione ci metti le parentesi {deleteElement()}
+                    Ma come facciamo a passargli l'ID se così è buggato?
+                */}
+                <button type="button" className="functionButtons-button" onClick={deleteElement}>
                     <i className="functionButtons-icon bi bi-x-lg"></i>
                 </button> 
         </div>  
@@ -21,4 +35,3 @@ function FunctionButtons() {
 }
 
 //https://formatjs.io/docs/getting-started/installation/
-export default FunctionButtons;
