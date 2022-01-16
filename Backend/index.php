d80b35c1527a24b2e09649cf30835c1c7487c6d6
Tommaso De Tommaso <thethunderingmarmot@tuta.io>
2022-01-16 22:05:22 +0100

    Fixed backend to send dates correctly.

--- Backend/index.php
+++ Backend/index.php
@@ -151,7 +151,7 @@
                 while($row = mysqli_fetch_row($this->lastResult))
                     $typesArray[$row[0]] = $row[1];
                 foreach($data as $key => $value)
-                    if($typesArray[$key] == "varchar")
+                    if($typesArray[$key] == "varchar" || $typesArray[$key] == "date")
                         $data[$key] = "\"" . $data[$key] . "\"";
                 
                 return $data;
