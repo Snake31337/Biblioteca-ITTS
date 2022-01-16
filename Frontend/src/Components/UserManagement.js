4213b946ec01958b29addcb7a2aa6a4c23843a79
rhiss <loumb31@gmail.com>
2022-01-16 10:11:34 +0100

    fixed styling

--- Frontend/src/Components/UserManagement.js
+++ Frontend/src/Components/UserManagement.js
@@ -1,28 +1,33 @@
 import React, { useState } from 'react'
 
-import '../CSS/UserManagement.scss'
-import '../CSS/Home.scss';
 import NavBar from './Navbar';
 import UserTable from './UserTable';
 import UserForm from './UserForm'
 import Modal from './Modal'
+import SideNavBar from './SideNavBar';
+
+import '../CSS/UserManagement.scss'
+import '../CSS/Home.scss';
+import '../CSS/Button.scss'
 
 export default function UserManagement() {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <div>
-            <div className="navBar-container">
-                <NavBar />
+            <div className="grid-item main">
+                <div className='main-wrapper'>
+                        <h1 className="title">Utenti</h1>
+                        <button className='button rounded' onClick={() => {setIsOpen(true); console.log("Aperto il Popup");}}>
+                            <div className="label">Aggiungi Utente</div>
+                            <i class="bi bi-plus-circle icon"></i>
+                        </button>
+                        <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+                            <UserForm />
+                        </Modal>
+        
+                        <UserTable className="main-item" />
+                    </div>
+                </div>
             </div>
-            <div className="centered-media">
-                <h1 className="title">Utenti</h1>
-                <button onClick={() => {setIsOpen(true); console.log("Aperto il Popup");}}>Aggiungi Nuovo Utente</button>
-                <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-                    <UserForm />
-                </Modal>
-
-                <UserTable />
-            </div>
-        </div>
     );
 }
